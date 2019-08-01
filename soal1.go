818a9137ea65327a7a843d39555b3906bf1694ba
eko <febri1412@gmail.com>
2019-08-01 16:03:53 +0700

    no. 1

---
+++ soal1.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func sorter(inputan string) string {
+	arr := strings.Split(inputan, "")
+	vocal := "aAiIuUeEoO"
+	hurufHidup := []string{}
+	hurufMati := []string{}
+	huruf := []string{}
+	for i := 0; i < len(arr); i++ {
+		if strings.Contains(vocal, arr[i]) {
+			hurufHidup = append(hurufHidup, arr[i])
+		} else {
+			hurufMati = append(hurufMati, arr[i])
+		}
+	}
+	sort.Strings(hurufHidup)
+	sort.Strings(hurufMati)
+	huruf = append(hurufHidup, hurufMati...)
+	return strings.Join(huruf, "")
+	//fmt.Println(huruf)
+}
+
+func main() {
+	fmt.Println(sorter("omama"))
+	fmt.Println(sorter("osama"))
+
+}
